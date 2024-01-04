e0041f9db8edbab18a38cf5ed431f44d7fac4e82
Pascal Garber <pascal@artandcode.studio>
2024-01-04 15:31:37 +0100

    Add basic permissions class

--- core/ops.ts
+++ core/ops.ts
@@ -1,7 +1,15 @@
 import { ResourceTable } from "./resources.ts";
 
+// TODO move to right place, maybe ./packages/deno/runtime-2/src/runtime/ops/permissions.rs ?
+class Permissions {
+  checkNetUrl(url: string, apiName: string): boolean {
+    return true;
+  }
+}
+
 class OpState {
-  resource_table = new ResourceTable();
+  public resourceTable = new ResourceTable();
+  public permissions = new Permissions();
 }
   
 export const state = new OpState();
--- core/resources.ts
+++ core/resources.ts
@@ -7,57 +7,67 @@
 export type ResourceId = number;
 
 export class ResourceTable {
-    resources: Map<ResourceId, any> = new Map();
-  
-    /**
-     * Adds a resource to the resource table and returns its `rid`.
-     */
-    add<T>(resource: T): ResourceId {
-      let rid = this.resources.size;
-      this.resources.set(rid, resource);
-      return rid;
+  resources: Map<ResourceId, any> = new Map();
+
+  /**
+   * Adds a resource to the resource table and returns its `rid`.
+   */
+  add<T = any>(resource: T): ResourceId {
+    let rid = this.resources.size;
+    this.resources.set(rid, resource);
+    return rid;
+  }
+
+  take<T = any>(rid: ResourceId): T {
+    let resource = this.resources.get(rid);
+    if (resource === undefined) {
+      throw new Error(`Resource ${rid} not found`);
     }
-  
-    take<T>(rid: ResourceId): T {
-      let resource = this.resources.get(rid);
-      if (resource === undefined) {
-        throw new Error(`Resource ${rid} not found`);
-      }
-      this.resources.delete(rid);
-      return resource;
+    this.resources.delete(rid);
+    return resource;
+  }
+
+  /**
+   * Returns a reference of type `T` with the given `rid`.
+   * @param rid
+   * @returns
+   */
+  get<T = any>(rid: ResourceId): T {
+    let resource = this.resources.get(rid);
+    if (resource === undefined) {
+      throw new Error(`Resource ${rid} not found`);
     }
-  
-    /**
-     * Returns a reference of type `T` with the given `rid`.
-     * @param rid
-     * @returns
-     */
-    get<T>(rid: ResourceId): T {
-      let resource = this.resources.get(rid);
-      if (resource === undefined) {
-        throw new Error(`Resource ${rid} not found`);
-      }
-      return resource;
+    return resource;
+  }
+
+  /**
+   * Returns true if any resource with the given `rid` exists.
+   * @param rid
+   * @returns
+   */
+  has(rid: ResourceId): boolean {
+    return this.resources.has(rid);
+  }
+
+  /**
+   * Replaces a resource with a new resource.
+   * Panics if the resource does not exist.
+   */
+  replace<T = any>(rid: ResourceId, resource: T): void {
+    if (!this.resources.has(rid)) {
+      throw new Error(`Resource ${rid} not found`);
     }
-  
-    /**
-     * Returns true if any resource with the given `rid` exists.
-     * @param rid
-     * @returns
-     */
-    has(rid: ResourceId): boolean {
-      return this.resources.has(rid);
+    this.resources.set(rid, resource);
+  }
+
+  /**
+   * Removes a resource from the resource table.
+   * Panics if the resource does not exist.
+   */
+  remove(rid: ResourceId): void {
+    if (!this.resources.has(rid)) {
+      throw new Error(`Resource ${rid} not found`);
     }
-  
-    /**
-     * Replaces a resource with a new resource.
-     * Panics if the resource does not exist.
-     */
-    replace<T>(rid: ResourceId, resource: T): void {
-      if (!this.resources.has(rid)) {
-        throw new Error(`Resource ${rid} not found`);
-      }
-      this.resources.set(rid, resource);
-    }
-  
+    this.resources.delete(rid);
+  }
 }
